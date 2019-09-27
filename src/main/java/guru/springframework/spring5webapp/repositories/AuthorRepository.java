5ee2e1773d6f26f76b1b8e48f8160fefcd579c4a
Ralph L <lettau.ralph@gmail.com>
2019-09-27 17:33:55 +0200

    Initalizing Data with Spring Framework Events

--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5webapp.model.repository;
+package guru.springframework.spring5webapp.repositories;
 
 import guru.springframework.spring5webapp.model.Author;
 import org.springframework.data.repository.CrudRepository;
